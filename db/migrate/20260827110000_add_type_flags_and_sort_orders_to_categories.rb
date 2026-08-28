9793e5f742e74e73f271a26039fee2525625df86
Anderson A Lopes <romalopes@yahoo.com.br>
2026-08-28 16:33:24 +1000

    temporary

--- db/migrate/20260827110000_add_type_flags_and_sort_orders_to_categories.rb
+++ db/migrate/20260827110000_add_type_flags_and_sort_orders_to_categories.rb
@@ -1,8 +1,4 @@
 class AddTypeFlagsAndSortOrdersToCategories < ActiveRecord::Migration[7.1]
-  class MigrationAddTypeFlagsAndSortOrdersToCategories < ActiveRecord::Base
-    self.table_name = "categories"
-  end
-
   def up
     change_table :categories do |t|
       t.boolean :for_wine, default: false, null: false
@@ -12,30 +8,7 @@ class AddTypeFlagsAndSortOrdersToCategories < ActiveRecord::Migration[7.1]
       t.integer :sort_order_review
       t.integer :sort_order_article
     end
-
-    MigrationAddTypeFlagsAndSortOrdersToCategories.reset_column_information
-
-    MigrationAddTypeFlagsAndSortOrdersToCategories.create(name: "Tastings", slug: "tastings") unless MigrationAddTypeFlagsAndSortOrdersToCategories.find_by(name: "Tastings") # TASTINGS
-    MigrationAddTypeFlagsAndSortOrdersToCategories.create(name: "Australian Icons", slug: "australian-icons") unless MigrationAddTypeFlagsAndSortOrdersToCategories.find_by(name: "Australian Icons")
-    MigrationAddTypeFlagsAndSortOrdersToCategories.create(name: "Interviews", slug: "interviews") unless MigrationAddTypeFlagsAndSortOrdersToCategories.find_by(name: "Interviews")
-    MigrationAddTypeFlagsAndSortOrdersToCategories.create(name: "Australian Chardonnay | Best Reviewed", slug: "australian-chardonnay-best-reviewed") unless MigrationAddTypeFlagsAndSortOrdersToCategories.find_by(name: "Australian Chardonnay | Best Reviewed")
-    MigrationAddTypeFlagsAndSortOrdersToCategories.create(name: "Eno Travel", slug: "eno-travel") unless MigrationAddTypeFlagsAndSortOrdersToCategories.find_by(name: "Eno Travel")
-    MigrationAddTypeFlagsAndSortOrdersToCategories.create(name: "Regional Tastings", slug: "regional-tastings") unless MigrationAddTypeFlagsAndSortOrdersToCategories.find_by(name: "Regional Tastings")
-    MigrationAddTypeFlagsAndSortOrdersToCategories.create(name: "Producer Spotlight", slug: "producer-spotlight") unless MigrationAddTypeFlagsAndSortOrdersToCategories.find_by(name: "Producer Spotlight")
-
-    # Existing categories were article categories; give them incremental
-    # sort orders in their current order of existence.
-    MigrationAddTypeFlagsAndSortOrdersToCategories.order(:id).each_with_index do |category, index|
-      MigrationAddTypeFlagsAndSortOrdersToCategories.update_columns(
-        for_article: true,
-        sort_order_wine: index + 1,
-        sort_order_review: index + 1,
-        sort_order_article: index + 1
-      )
-    end
   end
-
-
 
   def down
     remove_columns :categories, :for_wine, :for_article, :for_review,
