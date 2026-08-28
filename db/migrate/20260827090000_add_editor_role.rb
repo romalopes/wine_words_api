175e64ed8a2b213ed9075c07b7021b848c7df598
Anderson A Lopes <romalopes@yahoo.com.br>
2026-08-28 16:16:02 +1000

    temporary

--- db/migrate/20260827090000_add_editor_role.rb
+++ db/migrate/20260827090000_add_editor_role.rb
@@ -2,9 +2,6 @@ class AddEditorRole < ActiveRecord::Migration[7.1]
   def up
     Role.find_or_create_by!(name: "Editor")
 
-    change_column_default :producers, :producer_type, 0
-    # change_column_null :producers, :producer_type, false
-
     Category.create(name: "Tastings", slug: "tastings") unless Category.find_by(name: "Tastings") # TASTINGS
     Category.create(name: "Australian Icons", slug: "australian-icons") unless Category.find_by(name: "Australian Icons")
     Category.create(name: "Interviews", slug: "interviews") unless Category.find_by(name: "Interviews")
