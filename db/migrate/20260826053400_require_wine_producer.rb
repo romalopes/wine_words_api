9555a650b43fe3b92b089e4f712b3981c415708b
Anderson A Lopes <romalopes@yahoo.com.br>
2026-08-28 15:58:42 +1000

    temporary

--- db/migrate/20260826053400_require_wine_producer.rb
+++ db/migrate/20260826053400_require_wine_producer.rb
@@ -1,11 +1,12 @@
 class RequireWineProducer < ActiveRecord::Migration[7.1]
   def up
     # Backfill wines without a producer using the first producer in the DB.
+    Producer.first || Producer.create!(name: "Unknown Producer", email: "unknown@unknown")
     if Producer.none?
       raise ActiveRecord::IrreversibleMigration,
             "Cannot make wines.producer_id NOT NULL: there are no producers to assign."
     end
-    Producer.first || Producer.create!(name: "Unknown Producer", email: "unknown@unknown")
+
     fallback_producer_id = Producer.first.id
     Wine.where(producer_id: nil).find_each do |wine|
       wine.update_column(:producer_id, fallback_producer_id)
