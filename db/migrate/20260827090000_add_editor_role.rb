8d6690e70e31b21631d08c21c02a8c8c9bb58a7e
Anderson A Lopes <romalopes@yahoo.com.br>
2026-08-28 16:18:56 +1000

    temporary

--- db/migrate/20260827090000_add_editor_role.rb
+++ db/migrate/20260827090000_add_editor_role.rb
@@ -1,14 +1,6 @@
 class AddEditorRole < ActiveRecord::Migration[7.1]
   def up
     Role.find_or_create_by!(name: "Editor")
-
-    Category.create(name: "Tastings", slug: "tastings") unless Category.find_by(name: "Tastings") # TASTINGS
-    Category.create(name: "Australian Icons", slug: "australian-icons") unless Category.find_by(name: "Australian Icons")
-    Category.create(name: "Interviews", slug: "interviews") unless Category.find_by(name: "Interviews")
-    Category.create(name: "Australian Chardonnay | Best Reviewed", slug: "australian-chardonnay-best-reviewed") unless Category.find_by(name: "Australian Chardonnay | Best Reviewed")
-    Category.create(name: "Eno Travel", slug: "eno-travel") unless Category.find_by(name: "Eno Travel")
-    Category.create(name: "Regional Tastings", slug: "regional-tastings") unless Category.find_by(name: "Regional Tastings")
-    Category.create(name: "Producer Spotlight", slug: "producer-spotlight") unless Category.find_by(name: "Producer Spotlight")
   end
 
   def down
