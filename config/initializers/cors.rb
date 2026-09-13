a0448398e11140679f0c5c32c9798a82dfdd30d1
Anderson A Lopes <romalopes@yahoo.com.br>
2026-09-13 17:19:40 +1000

    cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -13,7 +13,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     # flow) cross-origin. The explicit origin (not "*") is required by the CORS
     # spec when credentials are allowed.
     # origins "http://localhost:5173"
-    origins "http://localhost:5173", "https://wine-prediction-app.vercel.app", "https://wine-prediction-mu.vercel.app"
+    origins "http://localhost:5173", "https://wine-prediction-app.vercel.app", "http://wine-prediction-mu.vercel.app", "https://wine-prediction-mu.vercel.app"
 
     resource "*",
       headers: :any,
